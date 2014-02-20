74bd69c38ed277ed978e427fb3c777b52ef55799
baugh70 <zakkyzakkba@gmail.com>
2014-02-20 12:33:37 -0500

    Update Legions.java

--- Legions.java
+++ Legions.java
@@ -1,4 +1,9 @@
 public class Legions{
+  
+  public void onEnable(){
+    getCommand("command").setExecutor(new LegionCommandExecutor);
+    getCommand("command").setExecutor(new PlayerCommandExecutor);
+  )
 
 
 }
